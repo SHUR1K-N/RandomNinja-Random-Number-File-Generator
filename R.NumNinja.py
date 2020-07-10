c1c5085d97d6b58246876d5f91be9f80bdef4054
Aftab 'SHUR1KEN' Alam <42811989+SHUR1K-N@users.noreply.github.com>
2020-07-11 02:32:56 +0530

    Fixed ZeroDivisionError exception

--- R.NumNinja.py
+++ R.NumNinja.py
@@ -78,7 +78,11 @@ while (correctInput is False):
         elif (minunit > maxunit):
             print("\nThe minimum value cannot be greater than the maximum value.")
     except ZeroDivisionError:
+        print("\n\nThe task completed successfully in zero seconds.")
+        print("Press any key to exit.")
+        input()
         break
     except:
+        print(e)
         print("\nOne of more of the inputs are invalid. This can happen when any spaces or other characters have been entered instead of numbers. Please try again.")
         continue
