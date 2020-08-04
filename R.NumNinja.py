fbf96f029a9589f5446c938e8fbe0db42f98d235
Aftab 'SHUR1KEN' Alam <42811989+SHUR1K-N@users.noreply.github.com>
2020-08-04 16:38:55 +0530

    Orgnized banner into function and added dunder variable for the sake of convention/safety

--- R.NumNinja.py
+++ R.NumNinja.py
@@ -6,9 +6,7 @@ import random
 
 correctInput = False
 
-init()
-
-print(colored('''
+BANNER1 = colored('''
 
              ██▀███        ███▄    █  █    ██  ███▄ ▄███▓ ███▄    █  ██▓ ███▄    █  ▄▄▄██▀▀▀▄▄▄
             ▓██ ▒ ██▒      ██ ▀█   █  ██  ▓██▒▓██▒▀█▀ ██▒ ██ ▀█   █ ▓██▒ ██ ▀█   █    ▒██  ▒████▄
@@ -20,9 +18,14 @@ print(colored('''
               ░░   ░  ░      ░   ░ ░  ░░░ ░ ░ ░      ░      ░   ░ ░  ▒ ░   ░   ░ ░  ░ ░ ░    ░   ▒
                ░       ░           ░    ░            ░            ░  ░           ░  ░   ░        ░  ░
                        ░
-''', 'blue'))
-print(colored('''                                    Random NumNinja: The Random Number File Generator''', 'red'))
-print(colored('''                                   ---------------------------------------------------''', 'blue'))
+''', 'blue')
+BANNER2 = colored('''                                Random NumNinja: The Random Number File Generator''', 'red')
+BANNER3 = colored('''                               ---------------------------------------------------''', 'blue')
+
+
+def printBanner():
+    init()
+    print(BANNER1), print(BANNER2), print(BANNER3)
 
 
 ########## Randomizer ###########
@@ -48,68 +51,72 @@ def generateUnique(minunit, maxunit, top):
             file.write(randNum)
     return()
 
+
 ############### Main ###############
 
+if __name__ == "__main__":
 
-while (correctInput is False):
-    try:
-        minunit = int(input("\nEnter the minimum value (Default = zero): ") or '0')
-        maxunit = int(input("Enter the maximum value: "))
-        top = int(input("Enter the maximum number of lines to be generated: "))
+    printBanner()
 
-        if maxunit > minunit:
-            Output = str(input("Enter output folder (Default = working folder):") or "./")
-            Output += "/"
+    while (correctInput is False):
+        try:
+            minunit = int(input("\nEnter the minimum value (Default = zero): ") or '0')
+            maxunit = int(input("Enter the maximum value: "))
+            top = int(input("Enter the maximum number of lines to be generated: "))
 
-            print("\nAllow repeating values?")
-            print("1. Yes\n2. No")
-            uniquePrompt = input("\nSelect option number (Default = No): ") or "2"
+            if maxunit > minunit:
+                Output = str(input("Enter output folder (Default = working folder):") or "./")
+                Output += "/"
 
-            if uniquePrompt == "1":
-                print("\nShow progress?")
-                print("1. Yes (slower)\n2. No (faster)")
-                progressPrompt = input("\nSelect option number (Default = No): ") or "2"
+                print("\nAllow repeating values?")
+                print("1. Yes\n2. No")
+                uniquePrompt = input("\nSelect option number (Default = No): ") or "2"
 
-            Output += str(minunit) + " to " + str(maxunit) + " [Randomized].txt"
+                if uniquePrompt == "1":
+                    print("\nShow progress?")
+                    print("1. Yes (slower)\n2. No (faster)")
+                    progressPrompt = input("\nSelect option number (Default = No): ") or "2"
 
-            maxunit += 1                        # To include the maxunit integer as well (added this far in to not interfere with file naming string)
+                Output += str(minunit) + " to " + str(maxunit) + " [Randomized].txt"
 
-            check = (maxunit - minunit)
-            if (check >= top):
-                pass
-            elif (check < top):
-                top = (maxunit - minunit)
+                maxunit += 1                        # To include the maxunit integer as well (added this far in to not interfere with file naming string)
 
-            print("\nNumber of lines that will be generated: %d" % top)
-            print("\nWorking...", end='')
+                check = (maxunit - minunit)
+                if (check >= top):
+                    pass
+                elif (check < top):
+                    top = (maxunit - minunit)
 
-            if uniquePrompt == "1":
-                with open(Output, '+w') as file:
-                    start = time.time()
-                    generate(minunit, maxunit, top)
-                    completionTime = time.time() - start
-            elif uniquePrompt == "2":
-                with open(Output, '+w') as file:
-                    start = time.time()
-                    generateUnique(minunit, maxunit, top)
-                    completionTime = time.time() - start
-            file.close()
-            print("\n\nThe task completed successfully in %f seconds. (at ~%d lines/sec)" % (completionTime, top // completionTime))
+                print("\nNumber of lines that will be generated: %d" % top)
+                print("\nWorking...", end='')
+
+                if uniquePrompt == "1":
+                    with open(Output, '+w') as file:
+                        start = time.time()
+                        generate(minunit, maxunit, top)
+                        completionTime = time.time() - start
+                elif uniquePrompt == "2":
+                    with open(Output, '+w') as file:
+                        start = time.time()
+                        generateUnique(minunit, maxunit, top)
+                        completionTime = time.time() - start
+                file.close()
+                print("\n\nThe task completed successfully in %f seconds. (at ~%d lines/sec)" % (completionTime, top // completionTime))
+                print("Press any key to exit.")
+                input()
+
+                break
+
+            elif (minunit == maxunit):
+                print("\nThe minimum value cannot be equal to the maximum value.")
+
+            elif (minunit > maxunit):
+                print("\nThe minimum value cannot be greater than the maximum value.")
+        except ZeroDivisionError:
+            print("\n\nThe task completed successfully in zero seconds.")
             print("Press any key to exit.")
             input()
-
             break
-
-        elif (minunit == maxunit):
-            print("\nThe minimum value cannot be equal to the maximum value.")
-
-        elif (minunit > maxunit):
-            print("\nThe minimum value cannot be greater than the maximum value.")
-    except ZeroDivisionError:
-        print("\n\nThe task completed successfully in zero seconds.")
-        print("Press any key to exit.")
-        input()
-        break
-    except:
-        print("\nOne of more of the inputs are invalid. This can happen when any spaces or other characters have been entered instead of numbers. Please try again.\n")
-        continue
+        except:
+            print("\nOne of more of the inputs are invalid. This can happen when any spaces or other characters have been entered instead of numbers. Please try again.\n")
+            continue
